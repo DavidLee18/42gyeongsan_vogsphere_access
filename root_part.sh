2040182372cc789b5b7cc75449e44211f613b05d
DavidLee18 <djwodus@gmail.com>
2024-10-01 11:27:40 +0900

    basic copy from vogsphere

--- root_part.sh
+++ root_part.sh
@@ -4,45 +4,39 @@
 
 cat >> /etc/krb5.conf << EOF
 [libdefaults]
-	default_realm = 42.FR
+	default_realm = 42GYEONGSAN.KR
 	forwardable = true
 	proxiable = true
-      	dns_lookup_kdc = no
-      	dns_lookup_realm = no
+    dns_lookup_kdc = no
+    dns_lookup_realm = no
 	allow_weak_crypto = true
+	rdns = false
 
 [realms]
-	42.FR = {
-                kdc = kdc1.42.fr
-                kdc = kdc2.42.fr
-                admin_server = kdc1.42.fr
-		default_domain = 42.fr
+	42GYEONGSAN.KR = {
+        kdc = ldap.42gyeongsan.kr
+		default_domain = 42gyeongsan.kr
+		database_module = openldap_ldapconf
 		default_lifetime = 7d
-                ticket_lifetime = 7d
+        ticket_lifetime = 7d
 	}
-        STAFF.42.FR = {
-                kdc = dc1.staff.42.fr
-                admin_server = dc1.staff.42.fr
-                default_domain = staff.42.fr
-        }
 
 [domain_realm]
-	.42.fr = 42.FR
-	42.fr = 42.FR
-	.staff.42.fr = STAFF.42.FR
-	staff.42.fr = STAFF.42.FR
+	.42gyeongsan.kr = 42GYEONGSAN.KR
+	42gyeongsan.kr = 42GYEONGSAN.KR
 EOF
 
 ### We set up the ssh config file to access to 42's git server (vogsphere)
 ### using your kerberos identification
 
 cat >> /etc/ssh/ssh_config << EOF
-Host *.42.fr
+Host *.42gyeongsan.kr
      SendEnv LANG LC_*
      StrictHostKeyChecking no
      GSSAPIAuthentication yes
      GSSAPIDelegateCredentials yes
      PasswordAuthentication yes
+     HashKnownHosts yes
 EOF
 
 echo "root part finished !"
--- user_part.sh
+++ user_part.sh
@@ -4,7 +4,7 @@
 
 mkdir -p ~/.ssh &&
 cat >> ~/.ssh/config << EOF
-Host vgs vgs.42.fr vgse vgse.42.fr vogsphere vogsphere.42.fr vogsphere-exam vogsphere-exam.42.fr
+Host vogsphere vogsphere.42gyeongsan.kr
 	User vogsphere
 EOF
 
